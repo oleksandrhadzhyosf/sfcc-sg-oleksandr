10b8d3f670134ec9000c59b11d5638a4e5d2ca7d
oleksandrhadzhyosf <78547642+oleksandrhadzhyosf@users.noreply.github.com>
2021-04-01 10:13:24 +0300

    Added logic

--- app_storefront_training/cartridge/scripts/job/csvCategoryExportXML.js
+++ app_storefront_training/cartridge/scripts/job/csvCategoryExportXML.js
@@ -7,31 +7,94 @@ const app = require('app_storefront_controllers/cartridge/scripts/app');
 const CatalogMgr = require('dw/catalog/CatalogMgr');
 const ProductSearchModel = require('dw/catalog/ProductSearchModel');
 
+const File = require('dw/io/File');
+const FileWriter = require('dw/io/FileWriter');
+const FileReader = require('dw/io/FileReader');
+const CSVStreamWriter = require('dw/io/CSVStreamWriter');
+const CVStreamReader = require('dw/io/CSVStreamReader');
+const XMLStreamWriter = require('dw/io/XMLStreamWriter');
+
 function execute(args) { 
     let statusOk = true;
-    let productCategory = args.productCategory;
-    //let products = CatalogMgr.getCategory(args.productCategory).getProducts();
-    let productSearchModel = new ProductSearchModel();
-    productSearchModel.setCategoryID(args.productCategory);
-    productSearchModel.search();
-    let iterator = productSearchModel.productSearchHits;
-    let currentProduct;
-    while (iterator !== null && iterator.hasNext()) {
-        currentProduct = iterator.next();
-        let productId = currentProduct.getProductID();
-        let string = 'test';
-    }
-    try {
-        if (args.productCategory) {
-            let productCategory = args.productCategory;
-            let category = CatalogMgr.getCategory(args.productCategory);
+    if (args.productCategory) {
+        try {
+            let productSearchModel = new ProductSearchModel();
+            productSearchModel.setCategoryID(args.productCategory);
             productSearchModel.search();
-         //   let products = category.getProducts();
-            let string = 'test';
+
+            if (productSearchModel.count > 0) {
+                let csvFile = new File(File.IMPEX + File.SEPARATOR + 'csvProducts.csv');
+
+                if (!csvFile.exists()) {
+                    csvFile.createNewFile();
+                }
+
+                if (csvFile.exists()) {
+                    let iterator = productSearchModel.productSearchHits;
+                    let csvFileWriter = new FileWriter(csvFile);
+                    let csvw = new CSVStreamWriter(csvFileWriter);
+                    let i = 0;
+                    csvw.writeNext(['product name', 'product id', 'short description', 'product price']);
+                    while (iterator !== null && iterator.hasNext() && i < 10) {
+                        let currentProduct = iterator.next().getProduct();
+                        csvw.writeNext([currentProduct.getName(), currentProduct.getID(), currentProduct.getShortDescription().toString() , currentProduct.getPriceModel().getMaxPrice().getValue().toString()]);
+                        i++;
+                    }
+                    csvw.close();
+                    csvFileWriter.close();
+
+                    let stringList = null;
+                    let csvReader = new FileReader(csvFile);
+                    let csvr = new CVStreamReader(csvReader);
+
+                    if (csvr.readNext()) {
+                        let xmlFile = new File(File.IMPEX + File.SEPARATOR + 'csvProducts.xml');
+
+                        if (!xmlFile.exists()) {
+                            xmlFile.createNewFile();
+                        }
+
+                        if(xmlFile.exists()) {
+                            let xmlFileWriter = new FileWriter(xmlFile, 'UTF-8');
+                            let xsw = new XMLStreamWriter(xmlFileWriter);
+                            xsw.writeStartDocument();
+                            xsw.writeStartElement('catalog');
+                            xsw.writeAttribute('xmlns', 'http://www.demandware.com/xml/impex/catalog/2021-01-01');
+                            xsw.writeAttribute('catalog-id', 'storefront-catalog-m-en');
+                            xsw.writeAttribute('category-id', args.productCategory);
+                                xsw.writeStartElement("products");
+                                while (stringList = csvr.readNext()) {
+                                    let product = stringList;
+                                    xsw.writeStartElement("product");
+                                    xsw.writeAttribute("id", product[1]);
+                                        xsw.writeStartElement("name");
+                                            xsw.writeCharacters(product[0]);
+                                        xsw.writeEndElement();
+                                        xsw.writeStartElement("description");
+                                            xsw.writeCharacters(product[2]);
+                                        xsw.writeEndElement();
+                                        xsw.writeStartElement("price");
+                                            xsw.writeCharacters(product[3]);
+                                        xsw.writeEndElement();
+                                    xsw.writeEndElement();
+                                }
+                                xsw.writeEndElement();
+                            xsw.writeEndElement();
+                            xsw.writeEndDocument();
+                            xsw.close();
+                            xmlFileWriter.close();
+                        }
+                    }
+                    csvr.close(); 
+                    csvReader.close();
+                }
+                
+            }
+
+        } catch (error) {
+            statusOk = false;
+            Logger.error('AutomaticCategoryAssignation.js > execute crashed on l.{0}. ERROR: {1}', error.lineNumber, error.message);
         }
-    } catch (error) {
-        statusOk = false;
-        Logger.error('AutomaticCategoryAssignation.js > execute crashed on l.{0}. ERROR: {1}', error.lineNumber, error.message);
     }
 
     if (statusOk) {
